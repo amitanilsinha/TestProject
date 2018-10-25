997556be031ea5925f168295b409fd2e331cd3ba
amitanilsinha <43570792+amitanilsinha@users.noreply.github.com>
2018-10-25 13:49:00 +0530

    Update Basic-WebApp.ps1

--- Basic-WebApp.ps1
+++ Basic-WebApp.ps1
@@ -8,18 +8,18 @@ New-AzureRmResourceGroup -Name yellowResourceGroup -Location $location
 
 # Create an App Service plan in Free tier.
 New-AzureRmAppServicePlan -Name $webappname -Location $location `
--ResourceGroupName myResourceGroup -Tier Free
+-ResourceGroupName yellowResourceGroup -Tier Free
 
 # Create a web app.
 New-AzureRmWebApp -Name $webappname -Location $location `
--AppServicePlan $webappname -ResourceGroupName myResourceGroup
+-AppServicePlan $webappname -ResourceGroupName yellowResourceGroup
 
 # Upgrade App Service plan to Standard tier (minimum required by deployment slots)
-Set-AzureRmAppServicePlan -Name $webappname -ResourceGroupName myResourceGroup `
+Set-AzureRmAppServicePlan -Name $webappname -ResourceGroupName yellowResourceGroup `
 -Tier Standard
 
 #Create a deployment slot with the name "staging".
-New-AzureRmWebAppSlot -Name $webappname -ResourceGroupName myResourceGroup `
+New-AzureRmWebAppSlot -Name $webappname -ResourceGroupName yellowResourceGroup `
 -Slot staging
 
 # Configure GitHub deployment to the staging slot from your GitHub repo and deploy once.
@@ -27,10 +27,10 @@ $PropertiesObject = @{
     repoUrl = "$gitrepo";
     branch = "master";
 }
-Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName myResourceGroup `
+Set-AzureRmResource -PropertyObject $PropertiesObject -ResourceGroupName yellowResourceGroup `
 -ResourceType Microsoft.Web/sites/slots/sourcecontrols `
 -ResourceName $webappname/staging/web -ApiVersion 2015-08-01 -Force
 
 # Swap the verified/warmed up staging slot into production.
-Switch-AzureRmWebAppSlot -Name $webappname -ResourceGroupName myResourceGroup `
+Switch-AzureRmWebAppSlot -Name $webappname -ResourceGroupName yellowResourceGroup `
 -SourceSlotName staging -DestinationSlotName production
